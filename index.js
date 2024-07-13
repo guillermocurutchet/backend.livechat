0f42b52e3d8b16caa589b6f5b589f69f4d2c78fb
guillermocurutchet <guillermo.curutchet@opennube.com>
2024-07-13 18:38:48 -0300

    3

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
-const messageHandler = require('./functions/message');
+const messageHandler = require('./netlify/functions/message');
 
 const app = express();
 const port = process.env.PORT || 3000;
